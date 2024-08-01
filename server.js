55f89da1ccbe3a867be490f7ce86425a4e4d143d
Keshav-2601 <keshavv857@gmail.com>
2024-08-01 16:31:50 +0100

    git122

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 const server=express();
 const corsOptions = {
-    origin: 'https://greensapper.netlify.app', // Replace with your frontend URL
+    origin: 'https://greensapper.netlify.app/Contacts', // Replace with your frontend URL
     optionsSuccessStatus: 200
   };
-server.use(cors()); 
+server.use(cors(corsOptions),contactrouter); 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json()); 
 server.use('/service',servicerouter);
